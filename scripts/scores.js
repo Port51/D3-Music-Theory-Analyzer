0ca1ba1c7a4d353f36f4fed8fcc40893298bee91
BFranz51 <bfranz@mail.usf.edu>
2018-08-25 10:55:01 -0400

    Remote update
    Working on laptop without admin rights or git bash. Doing one big update via web interface. Main changes:
    * Improved graph physics
    * Improved graph setup
    * Modified similarity algorithm based on more testing
    * Hover effects for piano notes and key selection
    * Nicer buttons and sliders

--- scripts/scores.js
+++ scripts/scores.js
@@ -4,13 +4,16 @@
 	Calculates score from 0.0 to 1.0 based on how similar two modes are
 */
 var score = (a, b, settings) => {
+    const debugPrintAll = true;
+
 	const comps = getCompromiseScore(a, b, settings);
 	const noteSimScore = comps.noteSim;
 	const addScore = comps.add;
 	const remScore = comps.rem;
 
-	const rScore = getRootCloseness(a, b);
-	const mScore = getMoodSimilarity_Tone(a, b);
+	//const rScore = getRootCloseness(a, b);
+	const rScore = (a.key === b.key) ? 1.0 : 0.0;
+	const mScores = getMoodSimilarity_Tone(a, b);
 	const iCounts = getMoodSimilarity_Intervals(a, b);
 	let iCountScore = 0.0;
 	if (iCounts.same > 0) {
@@ -26,29 +29,45 @@ var score = (a, b, settings) => {
 
 	// How different are variances in interval length? (openness)
 	let iVarScore = 1.0 / (1.0 + iCounts.varianceDiff);
-	
+
 	// Set weights
 	const wRootMissingPenalty = settings.wFactorRootMissing;
 	const wComp = settings.wFactorNoteSimilarity;
 	const wAdd = settings.wFactorAdd;
 	const wRem = settings.wFactorRemove;
 	const wRoot = settings.wFactorRootOffset;
-	const wMood = settings.wFactorMoodEstimation;
+	const wMoodHappy = settings.wFactorMoodHappy;
+	const wMoodBittersweet = settings.wFactorMoodBittersweet;
 	const wIntervalCount = settings.wFactorIntervalCount;
 	const wIntervalVarDiff = settings.wFactorIntervalOpenness;
-	
+
 	let total = (noteSimScore * wComp + (1.0 - wComp)) *
 				(addScore * wAdd + (1.0 - wAdd)) *
 				(remScore * wRem + (1.0 - wRem)) *
 				(rScore * wRoot + (1.0 - wRoot)) *
-				(mScore * wMood + (1.0 - wMood)) *
+				(mScores.simHappy * wMoodHappy + (1.0 - wMoodHappy)) *
+				(mScores.simBittersweet * wMoodBittersweet + (1.0 - wMoodBittersweet)) *
 				(iCountScore * wIntervalCount + (1.0 - wIntervalCount)) *
 				(iVarScore * wIntervalVarDiff + (1.0 - wIntervalVarDiff)) *
 				(comps.rootMissingPenalty * wRootMissingPenalty + (1.0 - wRootMissingPenalty));
 	if (total < 0.0) total = 0.0;
 	if (total > 1.0) total = 1.0;
 
-	return {score: total, comp: noteSimScore, root: rScore, mood: mScore};
+	if (debugPrintAll && (a.isUser || b.isUser)) {
+        console.log(a.name + " compared to " + b.name);
+        console.log("Score: " + total);
+        console.log("Note similarity: " + noteSimScore);
+        console.log("Add score: " + addScore);
+        console.log("Rem score: " + remScore);
+        console.log("Mood score - happy: " + mScores.simHappy);
+        console.log("Mood score - bittersweet: " + mScores.simBittersweet);
+        console.log("rScore: " + rScore);
+        console.log("Root missing: " + comps.rootMissingPenalty);
+        console.log("Interval count: " + iCountScore);
+        console.log("Interval variance: " + iVarScore);
+	}
+
+	return total;
 }
 
 var avgScores = (a, b) => {
