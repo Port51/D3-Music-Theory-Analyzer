d9cf358a6b6d1f9d834f6fea93fed5bedefd922a
BFranz51 <bfranz@mail.usf.edu>
2018-08-25 10:54:32 -0400

    Remote update
    Working on laptop without admin rights or git bash. Doing one big update via web interface. Main changes:
    * Improved graph physics
    * Improved graph setup
    * Modified similarity algorithm based on more testing
    * Hover effects for piano notes and key selection
    * Nicer buttons and sliders

--- scripts/mood.js
+++ scripts/mood.js
@@ -50,22 +50,27 @@ var getMoodTone = (a) => {
 	else if (a.c[6] && !a.c[7])
 		sadHappy -= 0.2; // lydian
 
-	// Bittersweet from conflicts
+	// Bittersweetness comes from conflicts
 	if (a.c[3] && a.c[11])
 		bittersweet += 0.25;
-	if (a.c[3] && a.c[8])
+	if (a.c[3] && a.c[9])
 		bittersweet += 0.25;
-	if (a.c[4] && a.c[7])
+    if (a.c[4] && a.c[1])
+		bittersweet += 0.25;
+	if (a.c[4] && a.c[8])
+		bittersweet += 0.25;
+    if (a.c[4] && a.c[6])
 		bittersweet += 0.25;
 	if (a.c[3] && a.c[4])
-		bittersweet += 0.25;
-	// But bluesy runs make it less bittersweet
+		bittersweet += 0.40; // extra!
+
+	// Bluesy runs make it less bittersweet
 	if (a.c[5] && a.c[6] && a.c[7])
-		bittersweet -= 0.25;
+		bittersweet -= 0.3333;
 	if (a.c[8] && a.c[9])
-		bittersweet -= 0.25;
+		bittersweet -= 0.3333;
 	if (a.c[10] && a.c[11])
-		bittersweet -= 0.25;
+		bittersweet -= 0.3333;
 
 	if (sadHappy < 0.0) sadHappy = 0.0;
 	if (sadHappy > 1.0) sadHappy = 1.0;
@@ -75,137 +80,9 @@ var getMoodTone = (a) => {
 	return {'sadHappy': sadHappy, 'bittersweet': bittersweet};
 }
 
-var getMoodStatic = (a) => {
-	// Sliding scales
-	let motion = 0.5,
-		happySad = 0.5;
-		
-	// Special properties 0-1
-	let aggression = 0.0;
-	let wonkiness = 0.0;
-	let definition = 1.0;
-	if (a.c[1]) aggression += 0.1;
-	if (a.c[6]) aggression += 0.5;
-	if (a.c[8]) aggression += 0.5;
-	if (a.c[10]) aggression += 0.1;
-
-	// Root
-	if (!a.c[0])
-		definition -= 0.9;
-	
-	// 2nds
-	if (a.c[1] && a.c[2]) {
-		// b2 + 2
-		wonkiness += 0.25;
-	} else if (a.c[1]) {
-		// b2
-		motion += 0.25;
-	} else if (a.c[2]) {
-		// 2
-		
-	} else {
-		// none
-		definition -= 0.333;
-	}
-	
-	// 3rds
-	if (a.c[3] && a.c[4]) {
-		// b3 + 3
-		motion += 0.2;
-		happySad -= 0.333;
-	} else if (a.c[3]) {
-		// b3
-		happySad -= 0.5;
-	} else if (a.c[4]) {
-		// 3
-		happySad += 0.5;
-	} else {
-		// none
-		definition -= 0.75;
-	}
-	
-	// 4th + 5th
-	if (a.c[5] && a.c[6] && a.c[7]) {
-		// 4 b5 5
-		wonkiness += 0.25;
-	} else if (a.c[5] && !a.c[6] && a.c[7]) {
-		// standard 4 5
-		wonkiness -= 0.25;
-	} else if (!a.c[5] && a.c[6] && a.c[7]) {
-		// lydian
-		motion += 0.2;
-		happySad -= 0.2;
-	} else if (a.c[5] && a.c[6] && !a.c[7]) {
-		// 4 b5
-		happySad -= 0.2;
-		
-	} else if (a.c[5]) {
-		// just 4
-		definition -= 0.75;
-	} else if (a.c[6]) {
-		// just b5
-		happySad -= 0.2;
-	} else if (a.c[7]) {
-		// just 5
-		definition -= 0.6;
-	} else {
-		// none
-		definition -= 1.0;
-	}
-	
-	// 6th
-	if (a.c[3] && a.c[4]) {
-		// b6 + 6
-		wonkiness += 0.5;
-	} else if (a.c[3]) {
-		// b6
-		happySad += 0.125;
-	} else if (a.c[4]) {
-		// 6
-		happySad -= 0.125;
-	} else {
-		// none
-		definition -= 0.25;
-	}
-	
-	// 7th
-	if (a.c[3] && a.c[4]) {
-		// b7 + 7
-		wonkiness += 0.4;
-	} else if (a.c[3]) {
-		// b7
-		
-	} else if (a.c[4]) {
-		// 7
-		
-	} else {
-		// none
-		motion -= 0.25;
-		definition -= 0.25;
-	}
-
-	if (happySad < 0.0) happySad = 0.0;
-	if (happySad > 1.0) happySad = 1.0;
-	if (motion < 0.0) motion = 0.0;
-	if (motion > 1.0) motion = 1.0;
-	if (definition < 0.0) definition = 0.0;
-	if (definition > 1.0) definition = 1.0;
-	if (wonkiness < 0.0) wonkiness = 0.0;
-	if (wonkiness > 1.0) wonkiness = 1.0;
-	if (aggression < 0.0) aggression = 0.0;
-	if (aggression > 1.0) aggression = 1.0;
-
-	return { 'happySad': happySad, 'motion': motion, 'definition': definition, 'wonkiness': wonkiness, 'aggression': aggression };
-	
-}
-
 var getMoodSimilarity_Tone = (a, b) => {
 	const msA = getMoodTone(a);
 	const msB = getMoodTone(b);
-	if (a.name == "C Aeolian")
-		console.log(msA.sadHappy)
-	if (a.isUser)
-		console.log("USER " + msA.sadHappy)
 
 	let simSadHappy = 1.0 - Math.abs(msB.sadHappy - msA.sadHappy) / 0.6;
 	if (simSadHappy < 0.0) simSadHappy = 0.0;
@@ -215,48 +92,13 @@ var getMoodSimilarity_Tone = (a, b) => {
 	// Power factor
 	simSadHappy = Math.pow(simSadHappy, 3.0);
 	simBittersweet = Math.pow(simBittersweet, 3.0);
-	
-	let score = simSadHappy * 1.0 + 0.0 * simBittersweet;
 
-	if (score < 0.0) score = 0.0;
-	if (score > 1.0) score = 1.0;
+	if (simSadHappy < 0.0) simSadHappy = 0.0;
+	if (simSadHappy > 1.0) simSadHappy = 1.0;
+	if (simBittersweet < 0.0) simBittersweet = 0.0;
+	if (simBittersweet > 1.0) simBittersweet = 1.0;
 
-	return score;
-}
-
-var getMoodSimilarity_Simple = (a, b) => {
-	let score = 1.0;
-	
-	const msA = getMoodStatic(a);
-	const msB = getMoodStatic(b);
-	
-	// Compare differences in traits
-	const traits = ['happySad', 'motion', 'definition', 'wonkiness', 'aggression'];
-	const traitCurve = [0, 0, 0, 1, 1];
-	const traitWeight = [3, 2, 1, 1, 2];
-	let diff = 0.0;
-	for (let i = 0; i < traits.length; ++i) {
-		let traitName = traits[i];
-		if (traitCurve[i] == 0) {
-			// Matters if above/below 0.5
-			// Use V curve w/ 0.5 at edges, 1.0 in center
-			const avg = (msA[traitName] + msB[traitName]) * 0.5 - 0.5;
-			diff += Math.abs(msA[traitName] - msB[traitName]) * (1.0 - Math.abs(avg)) * traitWeight[i];
-		} else {
-			// Simple difference
-			diff += Math.abs(msA[traitName] - msB[traitName]) * traitWeight[i];
-		}
-	}
-	score = 1.0 / (1.0 + Math.pow(diff, 3.0));
-
-	// Apply stretch cutoff
-	const stretchCutoff = 0.1;
-	score = Math.max(0.0, score - stretchCutoff) / (1.0 - stretchCutoff);
-
-	if (score < 0.0) score = 0.0;
-	if (score > 1.0) score = 1.0;
-	
-	return score;
+	return {'simHappy': simSadHappy, 'simBittersweet': simBittersweet};
 }
 
 var varianceI = (arr) => {
